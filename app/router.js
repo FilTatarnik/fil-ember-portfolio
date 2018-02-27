973de812cfe2cbfb9900d6cfe360309e800f9c3f
Filip Tatarnik <ftatarnik@gmail.com>
2018-02-27 11:10:31 -0600

    Playing with CSS, adding more routes, new routes need test

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('rentals');
+  this.route('languages');
+  this.route('social');
 });
 
 export default Router;
